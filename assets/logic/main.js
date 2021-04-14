63515acb30dd8e7c77dd55d84ac2f17100ca2dad
Your Name <alexsigala87@gmail.com>
2021-04-13 22:01:56 -0700

    utilized regex constructor to make a regex string containing the document name. needed a constructor bc the file name will always change and is store in a variable

--- assets/logic/main.js
+++ assets/logic/main.js
@@ -1,5 +1,24 @@
 let matchRQ = () => {
-    console.log("it changed");
+    //get the string version of the rq number
+    let currentRQ = document.getElementById("RQnum").textContent;
+    //get the name of the file you uploaded
+    let fileName = document.getElementById("exampleFormControlFile1").value;
+    //get the index of just the file name by looking past fakepath
+    const index = fileName.indexOf("fakepath") + 9;
+    //convert the old filename to just the filename without the preceding BS
+    fileName = fileName.substring(index);
+    console.log(fileName);
+    //this regex says to match you must begin with the file name listed at teh top of the page and end with it
+    //or you can end with an underscore
+    let regEx = new RegExp("\\b" + currentRQ + "(\\b|_)", "g");
+    console.log(regEx);
+    let match = regEx.test(fileName);
+
+    if (match === true) {
+        return;
+    } else {
+        alert("paperwork doesnt match");
+    }
 };
 document
     .getElementById("exampleFormControlFile1")
